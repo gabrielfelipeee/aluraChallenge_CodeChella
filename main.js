1dbb85818a3c240be9cb628d5df7e4fb3f339f6e
gabrielfelipeee <gabrielfeliperodrigues78@gmail.com>
2023-03-19 17:41:34 -0300

    Header mobile finalizado

---
+++ main.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector('.icon-menu');
+
+menu.addEventListener('click', () => {
+    const listMenuInvisible = document.querySelector('.list-menu');
+
+    listMenuInvisible.classList.toggle('list-menu-invisible');
+}) 
