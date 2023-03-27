f4c6408a2e11cb4437088cea7c9bfa4e4e9835c8
gabrielfelipeee <gabrielfeliperodrigues78@gmail.com>
2023-03-26 22:42:50 -0300

    Criando a página de informações

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 const menu = document.querySelector('.icon-menu');
-
 menu.addEventListener('click', () => {
     const listMenuInvisible = document.querySelector('.list-menu');
 
     listMenuInvisible.classList.toggle('list-menu-invisible');
-}) 
+})
+
+
+// Exibir perguntas frequentes
+const questions = document.querySelectorAll('.questions-list-item');
+questions.forEach( element => {
+    element.addEventListener('click', () => {
+        element.children[1].classList.toggle('item-response-closed');
+        element.children[0].children[0].classList.toggle('image-closed');
+    })
+})
